ab0b0ed7e4c48bf95c9e7f427d3a4377ef85e238
Sebastian A. Weiß <post@sebastian-weiss.de>
2013-03-31 20:32:48 +0200

    cleanup

--- src/main/java/de/wsdevel/components/plotter/FunctionToPlot.java
+++ src/main/java/de/wsdevel/components/plotter/FunctionToPlot.java
@@ -5,33 +5,29 @@ import java.awt.Color;
 /**
  * Created on 26.05.2006.
  * 
- * @author <a href="mailto:sweiss@weissundschmidt.de">Sebastian A. Weiss - Weiss
- *         und Schmidt, Mediale Systeme GbR</a>
+ * (c) 2005, Sebastian A. Weiss - All rights reserved.
+ * 
+ * @author <a href="mailto:post@sebastian-weiss.de">Sebastian A. Weiss</a>
  * @version $Author: sweissTFH $ -- $Revision: 1.1 $ -- $Date: 2006-05-26
  *          18:25:05 $
- * 
- * <br>
- *          (c) 2005, Weiss und Schmidt, Mediale Systeme GbR - All rights
- *          reserved.
- * 
  */
 public interface FunctionToPlot {
 
-    /**
-     * COMMENT.
-     * 
-     * @return {@link Color }
-     */
-    Color getColor();
+	/**
+	 * COMMENT.
+	 * 
+	 * @return {@link Color }
+	 */
+	Color getColor();
 
-    /**
-     * COMMENT.
-     * 
-     * @param x
-     *            <code>double</code>
-     * @return <code>double</code>>
-     */
-    double f(double x);
+	/**
+	 * COMMENT.
+	 * 
+	 * @param x
+	 *            <code>double</code>
+	 * @return <code>double</code>>
+	 */
+	double f(double x);
 
 }
 /*
